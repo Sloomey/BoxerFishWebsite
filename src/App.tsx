8755ccd300220364e666d95dc969d2ed802e8d6f
Mandle <mculle2@gmail.com>
2025-08-06 01:13:36 -0500

    Logo Push

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ import Home from './pages/Home'
 import News from './pages/News'
 import Music from './pages/Music'
 import Store from './pages/Store'
+import BandLogo from './assets/BandLogo.png'
 
 function App() {
   return (
-    <>
+    <>        
+        <a href="https://vite.dev" target="_blank">
+          <img src={BandLogo} className="logo" alt="Vite logo" style={{ width: '80px', height: 'auto' }} />
+        </a>
       <Router> 
         <nav>
           <Link to={PAGES.home}> Home </Link>
